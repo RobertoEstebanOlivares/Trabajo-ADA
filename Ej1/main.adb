1c85664ec32ddeb8d587afe8581c4a94c4c62bfe
Roberto Esteban <45260992+RobertoEstebanOlivares@users.noreply.github.com>
2021-05-07 10:25:52 +0200

    Add files via upload

---
+++ Ej1/main.adb
@@ -0,0 +1,9 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+procedure Main is
+begin
+  Put_Line("----Inicio del Programa Main----.");
+  delay 5.0;
+  Put_Line("----Fin del Programa Main----.");
+end Main;
